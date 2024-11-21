e0dba462f49f43b66cc459e82f97398aff410ffc
SaugatxWasti <wastisaugat217@gmail.com>
2024-11-21 14:17:11 +0545

    react

---
+++ src/message.tsx
@@ -0,0 +1,13 @@
+
+// Pascalcasting-to make function componenet
+function Message(){
+
+    const name='Saugat';
+   if (name) 
+   
+    return <h1> hlo {name}</h1>;
+    return <h1>Hlo world</h1>;
+// it is going to compliled down to jsx
+ }
+
+ export default Message;
