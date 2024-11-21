5ea6c1c3b9b069747118f1e268aefaadedbf65c0
SaugatxWasti <wastisaugat217@gmail.com>
2024-11-21 20:23:41 +0545

    update

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,40 @@
+import { MouseEvent } from "react";
+
+function ListGroup() {
+  let item = ["Nepal", "Kathmandu", "Syangja", "Chitwan"];
+  let selectindexed = 0;
+  //Event hnadler
+
+  const handleclick = (Event: MouseEvent) => console.log(Event);
+
+  /*if (item.length === 0)
+    return (
+      <>
+        <h1>List</h1>
+        <p>No item found</p>
+      </>
+    );*/
+
+  return (
+    <>
+      <h1>List</h1>
+      {/*{item.length ===0 ? <p>NO items found</p> :null}*/}
+      <ul className="list-group">
+        {item.map((item, index) => (
+          <li
+            className={
+              selectindexed === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={handleclick}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+export default ListGroup;
